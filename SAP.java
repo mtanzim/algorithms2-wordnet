46964ab283855c3f5951fdd8eb17dcc322ccadfc
Tanzim Mokammel <mtanzim@gmail.com>
2019-08-24 18:27:09 +0200

    start sap

---
+++ SAP.java
@@ -0,0 +1,72 @@
+import edu.princeton.cs.algs4.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class SAP {
+    private Topological topo;  // topological order
+    private Digraph G;
+
+    // constructor takes a digraph (not necessarily a DAG)
+    public SAP(Digraph G) {
+        if (G == null) {
+            throw new IllegalArgumentException("invalid arg");
+        }
+        this.G = G;
+        topo = new Topological(G);
+        StdOut.println(G);
+        StdOut.println(topo.isDAG());
+        StdOut.println(topo.order());
+
+
+    }
+
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w) {
+        return 0;
+    }
+
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w) {
+        int curAncestor = -1;
+        List<Integer> possibleAncestors = new Stack<Integer>();
+        for (int cur : topo.order()) {
+            possibleAncestors.add(cur);
+            if (cur == v || cur == w) {
+                break;
+            }
+        }
+        List<Integer> curList = new ArrayList<Integer>();
+        curList.add(v);
+        curList.add(w);
+        for (int cur : possibleAncestors) {
+            curList.add(cur);
+            StdOut.println(cur);
+            BreadthFirstDirectedPaths bfs = new BreadthFirstDirectedPaths(G, curList);
+            curList.remove(2);
+        }
+
+        return 0;
+    }
+
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        return 0;
+    }
+
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        SAP sap = new SAP(G);
+        sap.ancestor(8, 9);
+
+
+    }
+
+}
--- WordNet.java
+++ WordNet.java
@@ -9,6 +9,12 @@ public class WordNet {
         if (_synsets == null || _hypernyms == null) {
             throw new IllegalArgumentException("invalid arg");
         }
+        // TODO:
+        // Corner cases. Throw a java.lang.IllegalArgumentException in the following situations:
+        // Any argument to the constructor or an instance method is null
+        // The input to the constructor does not correspond to a rooted DAG.
+        // Any of the noun arguments in distance() or sap() is not a WordNet noun.
+
         synsets = _synsets;
         hypernyms = _hypernyms;
     }
@@ -23,16 +29,20 @@ public class WordNet {
         return false;
     }
 
-    // distance between nounA and nounB (defpublic int distance(String nounA, String
-    // a synset (second field of synsets.txt// in a shortest ancestral path (defined
-    // below)
+    // distance between nounA and nounB
+    public int distance(String nounA, String nounB) {
+        return 0;
+    }
+
+    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+    // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
         return "None";
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
-        WordNet w = new WordNet("asdad", "asdasd");
+        WordNet w = new WordNet("asdasd", "asdasd");
         w.sap("a", "b");
         StdOut.println("Going bottom");
     }
