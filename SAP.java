e9414fe9459fabe8d30fa59f46f7e98b8ebf5355
Tanzim Mokammel <mtanzim@gmail.com>
2019-08-26 16:21:49 +0200

    cp: add asssertions

--- SAP.java
+++ SAP.java
@@ -1,8 +1,5 @@
 import edu.princeton.cs.algs4.*;
 
-import java.util.List;
-import java.util.Stack;
-
 public class SAP {
     private Topological topo;  // topological order
     private Digraph G;
@@ -41,9 +38,6 @@ public class SAP {
     // length of shortest ancestral path between v and w; -1 if no such path
     private int common(int v, int w, boolean isDist) {
         boolean debug = false;
-        List<Integer> possibleAncestors = new Stack<Integer>();
-
-
         if (debug) StdOut.println("\nv: " + v + " w: " + w);
         BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(G, v);
         BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(G, w);
@@ -91,17 +85,36 @@ public class SAP {
         In in = new In(args[0]);
         Digraph G = new Digraph(in);
         SAP sap = new SAP(G);
-        StdOut.println("len: " + sap.length(3, 11));
-        StdOut.println("ancestor: " + sap.ancestor(3, 11));
+        int len;
+        int ancestor;
 
-        StdOut.println("len: " + sap.length(9, 12));
-        StdOut.println("ancestor: " + sap.ancestor(9, 12));
+        len = sap.length(3, 11);
+        ancestor = sap.ancestor(3, 11);
+        StdOut.println("len: " + len);
+        StdOut.println("ancestor: " + ancestor);
+        assert ancestor == 1;
+        assert len == 4;
 
-        StdOut.println("len: " + sap.length(7, 2));
-        StdOut.println("ancestor: " + sap.ancestor(7, 2));
+        len = sap.length(9, 12);
+        ancestor = sap.ancestor(9, 12);
+        StdOut.println("len: " + len);
+        StdOut.println("ancestor: " + ancestor);
+        assert ancestor == 5;
+        assert len == 3;
 
-        StdOut.println("len: " + sap.length(1, 6));
-        StdOut.println("ancestor: " + sap.ancestor(1, 6));
+        len = sap.length(7, 2);
+        ancestor = sap.ancestor(7, 2);
+        StdOut.println("len: " + len);
+        StdOut.println("ancestor: " + ancestor);
+        assert ancestor == 0;
+        assert len == 4;
+
+        len = sap.length(1, 6);
+        ancestor = sap.ancestor(1, 6);
+        StdOut.println("len: " + len);
+        StdOut.println("ancestor: " + ancestor);
+        assert ancestor == -1;
+        assert len == -1;
     }
 
 }
