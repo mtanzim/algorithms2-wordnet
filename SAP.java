2691654f9011f02450acbf215c607b7b830283e4
Tanzim Mokammel <mtanzim@gmail.com>
2019-08-26 17:10:07 +0200

    finish sap?

--- SAP.java
+++ SAP.java
@@ -1,11 +1,13 @@
 import edu.princeton.cs.algs4.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SAP {
     private Topological topo;  // topological order
     private Digraph G;
     private String strRep;
+    private static final int INFINITY = Integer.MAX_VALUE;
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
@@ -38,39 +40,49 @@ public class SAP {
 
     private int common(Iterable<Integer> v, Iterable<Integer> w, boolean isDist) {
         boolean debug = false;
+        int minDist = INFINITY;
+        int curMinAnc = -1;
         if (debug) StdOut.println("\nv: " + v + " w: " + w);
         BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(G, v);
         BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(G, w);
         for (int cur : topo.order()) {
             if (bfsV.hasPathTo(cur) && bfsW.hasPathTo(cur)) {
+                int dist = bfsW.distTo(cur) + bfsV.distTo(cur);
+                if (debug) StdOut.println("\ndist: " + dist);
                 if (debug) StdOut.println(cur + " : " + bfsV.pathTo(cur));
                 if (debug) StdOut.println(cur + " : " + bfsW.pathTo(cur));
-                int dist = bfsW.distTo(cur) + bfsV.distTo(cur);
-                if (debug) StdOut.println("dist: " + dist);
-                if (isDist) return dist;
-                return cur;
+                if (dist < minDist) {
+                    if (debug) StdOut.println("changing min dist to: " + dist);
+                    if (debug) StdOut.println("changing min anc to: " + cur);
+                    curMinAnc = cur;
+                    minDist = dist;
+                }
             }
         }
-        return -1;
+        if (isDist) {
+            if (minDist == INFINITY) return -1;
+            return minDist;
+        }
+        return curMinAnc;
     }
 
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        ArrayList<Integer> vIter = new ArrayList<Integer>();
-        ArrayList<Integer> wIter = new ArrayList<Integer>();
-        vIter.add(v);
-        wIter.add(w);
-        return common(vIter, wIter, true);
+        ArrayList<Integer> vI = new ArrayList<Integer>();
+        ArrayList<Integer> wI = new ArrayList<Integer>();
+        vI.add(v);
+        wI.add(w);
+        return common(vI, wI, true);
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-        ArrayList<Integer> vIter = new ArrayList<Integer>();
-        ArrayList<Integer> wIter = new ArrayList<Integer>();
-        vIter.add(v);
-        wIter.add(w);
-        return common(vIter, wIter, false);
+        ArrayList<Integer> vI = new ArrayList<Integer>();
+        ArrayList<Integer> wI = new ArrayList<Integer>();
+        vI.add(v);
+        wI.add(w);
+        return common(vI, wI, false);
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
@@ -84,12 +96,34 @@ public class SAP {
     }
 
     public static void main(String[] args) {
-        In in = new In(args[0]);
+//        In in = new In(args[0]);
+        String filename = "digraph25.txt";
+        StdOut.println(filename);
+        In in = new In(filename);
         Digraph G = new Digraph(in);
         SAP sap = new SAP(G);
+
+        // tests with digraph25.txt
+        ArrayList<Integer> vI = new ArrayList<Integer>();
+        ArrayList<Integer> wI = new ArrayList<Integer>();
+        vI.addAll(Arrays.asList(13, 23, 24));
+        wI.addAll(Arrays.asList(6, 16, 17));
+
         int len;
         int ancestor;
+        len = sap.length(vI, wI);
+        ancestor = sap.ancestor(vI, wI);
+        StdOut.println("len: " + len);
+        StdOut.println("ancestor: " + ancestor);
+        assert ancestor == 3;
+        assert len == 4;
 
+        // tests with digraph1.txt
+        filename = "digraph1.txt";
+        StdOut.println(filename);
+        in = new In(filename);
+        G = new Digraph(in);
+        sap = new SAP(G);
         len = sap.length(3, 11);
         ancestor = sap.ancestor(3, 11);
         StdOut.println("len: " + len);
