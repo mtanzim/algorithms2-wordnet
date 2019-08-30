e38749d860d22874c30f2cdcea162bdcf12a324a
Tanzim Mokammel <mtanzim@gmail.com>
2019-08-30 11:51:41 +0200

    finish outcast

--- Outcast.java
+++ Outcast.java
@@ -10,22 +10,40 @@ public class Outcast {
     }
 
     public String outcast(String[] nouns) {
-        return "";
+        int maxD = -1;
+        int xt = -1;
+        int distances[] = new int[nouns.length];
+        for (int i = 0; i < nouns.length; i++) {
+            int curSum = 0;
+            for (int k = 0; k < nouns.length; k++) {
+                if (k == i) continue;
+                curSum += wordNet.distance(nouns[i], nouns[k]);
+            }
+            if (curSum > maxD) {
+                maxD = curSum;
+                xt = i;
+            }
+        }
+
+        return nouns[xt];
     }
 
     public static void main(String[] args) {
+        boolean debug = false;
         WordNet wordnet = new WordNet(args[0], args[1]);
-//        WordNet wordnet = new WordNet("synsets.txt", "hypernyms.txt");
         Outcast outcast = new Outcast(wordnet);
         for (int t = 2; t < args.length; t++) {
             In in = new In(args[t]);
             String[] nouns = in.readAllStrings();
-//            StdOut.println(args[t] + ": " + outcast.outcast(nouns));
-            StdOut.println(args[t]);
-            for (String noun : nouns) {
-                StdOut.print(noun + " ");
+            if (debug) {
+                StdOut.println(args[t]);
+                for (String noun : nouns) {
+                    StdOut.print(noun + " ");
+                }
+                StdOut.println();
             }
-            StdOut.println();
+
+            StdOut.println(args[t] + ": " + outcast.outcast(nouns));
         }
     }
 
