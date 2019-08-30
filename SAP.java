2f042d71c8d8ed17552399db552d371d12644791
Tanzim Mokammel <mtanzim@gmail.com>
2019-08-30 11:28:28 +0200

    clean up SAP.java

--- SAP.java
+++ SAP.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SAP {
-    //    private Topological topo;  // topological order, need this?
     private Digraph G;
     private String strRep;
     private static final int INFINITY = Integer.MAX_VALUE;
@@ -19,11 +18,9 @@ public class SAP {
             throw new IllegalArgumentException("invalid arg");
         }
         this.G = G;
-//        topo = new Topological(G);
         strRep = this.G.toString();
-        if (debug) StdOut.println(dotGen());
-//        if (debug) StdOut.println("topological order: " + topo.order());
-
+        if (debug)
+            StdOut.println(dotGen());
     }
 
     private String dotGen() {
@@ -45,31 +42,36 @@ public class SAP {
         boolean debug = false;
         int minDist = INFINITY;
         int curMinAnc = -1;
-        if (debug) StdOut.println("\nv: " + v + " w: " + w);
+        if (debug)
+            StdOut.println("\nv: " + v + " w: " + w);
         BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(G, v);
         BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(G, w);
-//        for (int cur : topo.order()) {
         for (int cur = 0; cur < G.V(); cur++) {
             if (bfsV.hasPathTo(cur) && bfsW.hasPathTo(cur)) {
                 int dist = bfsW.distTo(cur) + bfsV.distTo(cur);
-                if (debug) StdOut.println("\ndist: " + dist);
-                if (debug) StdOut.println(cur + " : " + bfsV.pathTo(cur));
-                if (debug) StdOut.println(cur + " : " + bfsW.pathTo(cur));
+                if (debug)
+                    StdOut.println("\ndist: " + dist);
+                if (debug)
+                    StdOut.println(cur + " : " + bfsV.pathTo(cur));
+                if (debug)
+                    StdOut.println(cur + " : " + bfsW.pathTo(cur));
                 if (dist < minDist) {
-                    if (debug) StdOut.println("changing min dist to: " + dist);
-                    if (debug) StdOut.println("changing min anc to: " + cur);
+                    if (debug)
+                        StdOut.println("changing min dist to: " + dist);
+                    if (debug)
+                        StdOut.println("changing min anc to: " + cur);
                     curMinAnc = cur;
                     minDist = dist;
                 }
             }
         }
         if (isDist) {
-            if (minDist == INFINITY) return -1;
+            if (minDist == INFINITY)
+                return -1;
             return minDist;
         }
         return curMinAnc;
     }
-
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
@@ -80,7 +82,8 @@ public class SAP {
         return common(vI, wI, true);
     }
 
-    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    // a common ancestor of v and w that participates in a shortest ancestral path;
+    // -1 if no such path
     public int ancestor(int v, int w) {
         ArrayList<Integer> vI = new ArrayList<Integer>();
         ArrayList<Integer> wI = new ArrayList<Integer>();
@@ -89,18 +92,19 @@ public class SAP {
         return common(vI, wI, false);
     }
 
-    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    // length of shortest ancestral path between any vertex in v and any vertex in
+    // w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
         return common(v, w, true);
     }
 
-    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    // a common ancestor that participates in shortest ancestral path; -1 if no such
+    // path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
         return common(v, w, false);
     }
 
     public static void main(String[] args) {
-//        In in = new In(args[0]);
         String filename = "digraph25.txt";
         StdOut.println(filename);
         In in = new In(filename);
